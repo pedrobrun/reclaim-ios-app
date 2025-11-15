f2e5a011f3e61a539ad19a3cd8fb465897cef37f
Pedro Brun <pedroluccavb@gmail.com>
2025-11-15 15:56:44 -0300

    Initial Commit

---
+++ Reclaim/ReclaimApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReclaimApp.swift
+//  Reclaim
+//
+//  Created by Pedro Brun on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ReclaimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
